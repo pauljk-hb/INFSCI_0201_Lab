fc67b6bf456c7f7c2ebab9f963748e28bae52c1a
Paul Kamp <115874788+pauljk-hb@users.noreply.github.com>
2024-09-11 12:06:16 -0400

    lab 1 error handling

--- Lab01/circle.py
+++ Lab01/circle.py
@@ -1,7 +1,10 @@
 import math
 
-
-radius = float(input("Enter the radius of the circle: "))
+try:
+	radius = float(input("Enter the radius of the circle: "))
+except ValueError:
+	print("Invalid input. Please enter a valid number for the radius.")
+	exit(1)
 area = math.pi * radius ** 2
 perimeter = 2 * math.pi * radius
 print(f'The circle with radius {radius} has an area of {area:.2f} and a perimeter of {perimeter:.2f}')
--- Lab01/unitconverter.py
+++ Lab01/unitconverter.py
@@ -1,26 +1,32 @@
 user_input = input("Enter a distance or a weight amount (Valid units in, cm, yd, m, oz, g,kg, lb): ")
 
 user_input = user_input.split(" ")
+
+# check if the input is valid
+if len(user_input) != 2:
+    print("Invalid input")
+    exit(1)
+
 user_unit_number = float(user_input[0])
 user_unit = user_input[1]
 
 # unit conversion
 if user_unit == "in":
-    print(f"{user_unit_number:.2f} in = {user_unit_number * 2.54} cm")
+    print(f"{user_unit_number:.2f} in = {(user_unit_number * 2.54):.2f} cm")
 elif user_unit == "cm":
-    print(f"{user_unit_number:.2f} cm = {user_unit_number / 2.54} in")
+    print(f"{user_unit_number:.2f} cm = {(user_unit_number / 2.54):.2f} in")
 elif user_unit == "yd":
-    print(f"{user_unit_number:.2f} yd = {user_unit_number * 0.9144} m")
+    print(f"{user_unit_number:.2f} yd = {(user_unit_number * 0.9144):.2f} m")
 elif user_unit == "m":
-    print(f"{user_unit_number:.2f} m = {user_unit_number / 0.9144} yd")
+    print(f"{user_unit_number:.2f} m = {(user_unit_number / 0.9144):.2f} yd")
 elif user_unit == "oz":
-    print(f"{user_unit_number:.2f} oz = {user_unit_number * 28.349523125} g")
+    print(f"{user_unit_number:.2f} oz = {(user_unit_number * 28.349523125):.2f} g")
 elif user_unit == "g":
-    print(f"{user_unit_number:.2f} g = {user_unit_number / 28.349523125} oz")
+    print(f"{user_unit_number:.2f} g = {(user_unit_number / 28.349523125):.2f} oz")
 elif user_unit == "lb":
-    print(f"{user_unit_number:.2f} lb = {user_unit_number * 0.45359237} kg")
+    print(f"{user_unit_number:.2f} lb = {(user_unit_number * 0.45359237):.2f} kg")
 elif user_unit == "kg":
-    print(f"{user_unit_number:.2f} kg = {user_unit_number / 0.45359237} lb")
+    print(f"{user_unit_number:.2f} kg = {(user_unit_number / 0.45359237):.2f} lb")
 else:
     print("Invalid unit")
     
